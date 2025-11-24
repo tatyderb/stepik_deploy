208e79c23e25ec7acba0b822e55dff865c48f91b
tatyderb <tatyderb@gmail.com>
2025-11-24 19:07:46 +0300

    Мешало нормально делать import

--- src/verification/__init__.py
+++ src/verification/__init__.py
@@ -1,4 +1,4 @@
 # src/verification/__init__.py
-from .snapshots_manager import SnapshotManager, main
-
-__all__ = ['SnapshotManager', 'main']
+# from .snapshots_manager import SnapshotManager, main
+#
+# __all__ = ['SnapshotManager', 'main']
